9b0f81b50700bd7fcbc89c8cffba95f34be8bd96
rrepo <reporepo254@gmail.com>
2021-08-08 19:29:00 +0900

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+
+    <Header>My Memos</Header>
+
+    <ul v-for ="memo in newest" :key="memo.id">
+      <li> <router-link :to ="{name :'Edit',params:{id:memo.id}}"> {{memo.body}} </router-link> </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import Header from "@/components/Header.vue"
+
+export default {
+  name:"home",
+  components:{
+    Header
+  },
+  computed:{
+    newest :function() {
+      return this.$store.state.memos.slice().reverse();
+    }
+  }
+}
+</script>
